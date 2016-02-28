6edf04dc3c2274414fa5fc172cd89d4041f27cda
Sestrenskyi-VO <sestrinskiy@mail.ru>
2016-02-28 12:22:54 +0200

    description of entity new table ORM

---
+++ 13-orm/create-entity/orm.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sestrenskyi-VO
+ * Date: 2016-02-28
+ * Time: 12:05
+ */
+namespace Academy\Orm13;
+
+use Bitrix\Main\Entity;
+
+class OrmTable extends Entity\DataManager {
+
+    /**
+     * @return string имя таблицы.  которую мы описуем
+     */
+    public static function getTableName(){
+        return 'academy_orm';
+    }
+
+    /**
+     * @return array описание полей таблицы
+     */
+    public static function getMap(){
+        return array(
+            new Entity\IntegerField('ID', array(
+                'primary' => true,     // поле обезательное для заполенения
+                'autocomplete' => true // автозаполения
+            )),
+            new Entity\StringField('ISBN', array(
+                'required' => true,
+            )),
+            new Entity\StringField('TITLE'),
+            new Entity\DataField('PUBLISH_DATE')
+        );
+    }
+}
+
+
+
